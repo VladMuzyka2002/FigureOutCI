b0ea0d436dc856ad647b0aba79e4d2e920ce42b7
Vlad Muzyka <angryvlad@gmail.com>
2024-10-14 18:25:25 -0400

    why is this not working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Means it is still not working
+          Means still is not working
         </p>
         <a
           className="App-link"
