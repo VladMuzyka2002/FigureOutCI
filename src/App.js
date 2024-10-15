813a7b79b2bfd5c49e23cfe9ee18b5bb8e89b67c
Vlad Muzyka <angryvlad@gmail.com>
2024-10-15 13:33:53 -0400

    i swear to god

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Means it is finally working
+          Under Construction
         </p>
         <a
           className="App-link"
